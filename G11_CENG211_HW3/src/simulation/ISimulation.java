fac64a16d962618495b43d274a8ffc4febc637a9
merveeMalak <73018652+merveeMalak@users.noreply.github.com>
2021-12-11 23:13:51 +0300

    Update ISimulation.java

--- G11_CENG211_HW3/src/simulation/ISimulation.java
+++ G11_CENG211_HW3/src/simulation/ISimulation.java
@@ -3,32 +3,31 @@ package simulation;
 public interface ISimulation {
 	
 	//calls needed methods in the needed order to simulate 5 days of the company
-	void simulateFiveDays();
-		
-	//creates game and movie critics and adds them to related queues
-	void createCriticQueues();
-	
-	//updates the content stacks for given day
-	void updateStacks(int dayNumber);
-	
-	//assigns movies at the stack to the critics at the queue
+    void simulateFiveDays();
+
+    //creates game and movie critics and adds them to related queues
+    void createQueuesAndStacks();
+
+    //updates the content stacks for given day
+    void updateStacks(int dayNumber);
+
+    //assigns movies at the stack to the critics at the queue
     //critics evaluates the movies
-	void evaluateMovies();
-	
-	//if there are any games to rate and available critics, assigns the games to critics
-	void evaluateNewGames();
-	
-	//makes critics continue their evaluations from where they left of
-	void continueEvaluatingGames();
-	
-	//prints evaluated games of the day and clears the daily lists after
-	void printDailyEvaluatedGames();
-	
-	//resets shifts of the critics for the new day
-	void resetShifts();
-	
-	//prints evaluated movies,games and their ratings
-	void printRatings();
+    void evaluateMovies();
+
+    //if there are any games to rate and available critics, assigns the games to critics
+    void evaluateNewGames();
+
+    //makes critics continue their evaluations from where they left of
+    void continueEvaluatingGames();
+
+    //prints evaluated games of the day and clears the daily lists after
+    void printDailyEvaluatedGames();
+
+    //resets shifts of the critics for the new day
+    void resetShifts();
+
+
 
 	
 	
