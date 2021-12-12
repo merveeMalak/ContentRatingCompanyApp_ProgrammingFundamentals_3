17380f3d228a064c40b25a0e61def44c5f6d334f
merveeMalak <73018652+merveeMalak@users.noreply.github.com>
2021-12-12 21:48:18 +0300

    Update ISimulation.java

--- src/simulation/ISimulation.java
+++ src/simulation/ISimulation.java
@@ -1,30 +1,21 @@
 package simulation;
 
-public interface ISimulation {
-	
-	//calls needed methods in the needed order to simulate 5 days of the company
-	void simulateFiveDays();
-		
-	//creates game and movie critics and adds them to related queues
-    void createQueuesAndStacks();
-	
-	//updates the content stacks for given day
-	void updateStacks(int dayNumber);
-	
-	//assigns movies at the stack to the critics at the queue
-    //critics evaluates the movies
-	void evaluateMovies();
-	
-	//if there are any games to rate and available critics, assigns the games to critics
-	void evaluateNewGames();
-	
-	//makes critics continue their evaluations from where they left of
-	void continueEvaluatingGames();
-	
-	//prints evaluated games of the day and clears the daily lists after
-	void printDailyEvaluatedGames();
-	
-	//resets shifts of the critics for the new day
-	void resetShifts();
 
+public interface ISimulation {
+
+    //calls needed methods in the needed order to simulate 5 days of the company
+    void simulateFiveDays();
+
+    //creates game and movie critics and adds them to related queues
+    void createQueuesAndStacks();
+
+    //updates the content stacks for given day
+    void updateStacks(int dayNumber);
+
+    //assigns movies at the stack to the critics at the queue
+    //critics evaluates the movies
+    void evaluateMovies();
+    
+    //if there are any games to rate and available critics, assigns the games to critics
+    public void evaluateNewGames();
 }
