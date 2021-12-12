7795353754947f75d119bbdec75a39ebfe2b8fda
merveeMalak <73018652+merveeMalak@users.noreply.github.com>
2021-12-12 21:38:40 +0300

    Update FileIO.java

--- src/fileIO/FileIO.java
+++ src/fileIO/FileIO.java
@@ -1,99 +1,101 @@
 package fileIO;
 
+import content.game.*;
+import content.movie.IMovie;
+import content.movie.Movie;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
-import content.game.CasualGame;
-import content.game.IGame;
-import content.game.IndefiniteGame;
-import content.game.StoryGame;
-import content.movie.IMovie;
-import content.movie.Movie;
+public class FileIO implements Serializable {
 
-public class FileIO {
-	
-	 private final String FILE_NAME = "contents.csv";
-	    private List<IMovie> movieArrayList;
-	    private List<IGame> gameArrayList;
+    private final String FILE_NAME = "contents.csv";
+    private List<IMovie> movieArrayList;
+    private List<IGame> gameArrayList;
 
-	    public FileIO() {
-	        this.movieArrayList = new ArrayList<IMovie>();
-	        this.gameArrayList = new ArrayList<IGame>();
-	    }
+    public FileIO() {
+      createLists();
+    }
 
-	    private void readFile() {
-	        try {
-	            Scanner scanner = new Scanner(new File(FILE_NAME));
-	            while (scanner.hasNextLine()) {
-	                String line = scanner.nextLine();
-	                if(!(line.equals(""))) {
-	                	decideContentType(line);
-	                }
-	            }
-	            scanner.close();
-	        }
-	        catch (FileNotFoundException e) {
-	            System.out.println("Error: "+e.getMessage());
-	        }
-	    }
+    private void createLists(){
+        this.movieArrayList = new ArrayList<IMovie>();
+        this.gameArrayList = new ArrayList<IGame>();
+        readFile();
+    }
+    private void readFile() {
+        try {
+            Scanner scanner = new Scanner(new File(FILE_NAME));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if(!(line.equals(""))) {
+                    decideContentType(line);
+                }
+            }
+            scanner.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
 
-	    private void decideContentType(String line) {
-	        String[] info = line.split(",");
-	        int contentId = Integer.parseInt(info[1]);
-	        switch (contentId) {
-	            case 0:
-	                IMovie movie = new Movie(info);
-	                movieArrayList.add(movie);
-	                break;
-	            case 1:
-	                IGame indefiniteGame = new IndefiniteGame(info);
-	                gameArrayList.add(indefiniteGame);
-	                break;
-	            case 2:
-	                IGame storyGame = new StoryGame(info);
-	                gameArrayList.add(storyGame);
-	                break;
-	            case 3:
-	                IGame casualGame = new CasualGame(info);
-	                gameArrayList.add(casualGame);
-	                break;
-	            default: break;
-	        }
+    private void decideContentType(String line) {
+        String[] info = line.split(",");
+        int contentId = Integer.parseInt(info[1]);
+        switch (contentId) {
+            case 0:
+                IMovie movie = new Movie(info);
+                movieArrayList.add(movie);
+                break;
+            case 1:
+                IGame indefiniteGame = new IndefiniteGame(info);
+                gameArrayList.add(indefiniteGame);
+                break;
+            case 2:
+                IGame storyGame = new StoryGame(info);
+                gameArrayList.add(storyGame);
+                break;
+            case 3:
+                IGame casualGame = new CasualGame(info);
+                gameArrayList.add(casualGame);
+                break;
+            default: break;
+        }
 
-	    }
+    }
 
-	    private List<IMovie> getMovieArrayList() {
-	        readFile();
-	        return movieArrayList;
-	    }
+    private List<IMovie> getMovieArrayList() {
+        return movieArrayList;
+    }
 
-	    public Stack<IMovie> getIndexDayOfMovie(int dayNumber){
-	        List<IMovie> movieList = getMovieArrayList();
-	        Stack<IMovie> movieStack = new Stack<IMovie>();
-	        for (IMovie movie: movieList) {
-	            if (movie.getArrivalDay() == dayNumber) {
-	                movieStack.add(movie);
-	            }
-	        }
-	        return movieStack;
-	    }
+    public Stack<IMovie> getIndexDayOfMovie(int dayNumber){
+        List<IMovie> movieList = getMovieArrayList();
+        Stack<IMovie> movieStack = new Stack<IMovie>();
+        for (IMovie movie: movieList) {
+            if (movie.getArrivalDay() == dayNumber) {
+                movieStack.add(movie);
+            }
+        }
+        return movieStack;
+    }
 
-	    public Stack<IGame> getIndexOfDayGame(int dayNumber){
-	        List<IGame> gameList = getGameArrayList();
-	        Stack<IGame> gameStack = new Stack<IGame>();
-	        for (IGame game: gameList) {
-	            if (game.getArrivalDay() == dayNumber) {
-	                gameStack.add(game);
-	            }
-	        }
-	        return gameStack;
-	    }
-	    private List<IGame> getGameArrayList() {
-	        return gameArrayList;
-	    }
+    public Stack<IGame> getIndexOfDayGame(int dayNumber){
+        List<IGame> gameList = getGameArrayList();
+        Stack<IGame> gameStack = new Stack<IGame>();
+        for (IGame game: gameList) {
+            if (game.getArrivalDay() == dayNumber) {
+                gameStack.add(game);
+            }
+        }
+        return gameStack;
+    }
+    private List<IGame> getGameArrayList() {
+        return gameArrayList;
+    }
+
 
 }
